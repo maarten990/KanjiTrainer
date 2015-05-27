b9ef9dfb3bcc7db4e1394c515ea9da56d6b71e34
Maarten de Jonge <mdejonge1990@gmail.com>
2015-05-27 16:58:32 +0200

    Revert "simplify run.sh (so that it actually works over ssh)"
    This reverts commit e875de0d4a249d0757bf7c46c6a59aa6137a065e.

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-pkill python
+PID_FILE=~/kanjitrainer.pid
+
+if [[ -f $PID_FILE ]]; then
+    kill $(cat $PID_FILE)
+fi
+
 python3 kanjitrainer.py &
+pid=$!
+echo $pid > ~/kanjitrainer.pid
