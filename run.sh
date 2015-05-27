e875de0d4a249d0757bf7c46c6a59aa6137a065e
Maarten de Jonge <mdejonge1990@gmail.com>
2015-05-27 16:52:14 +0200

    simplify run.sh (so that it actually works over ssh)

--- run.sh
+++ run.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-PID_FILE=~/kanjitrainer.pid
-
-if [[ -f $PID_FILE ]]; then
-    kill $(cat $PID_FILE)
-fi
-
+pkill python
 python3 kanjitrainer.py &
-pid=$!
-echo $pid > ~/kanjitrainer.pid
