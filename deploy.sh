be1a461bb0309ac1b008f77e54773a69d26efd74
Maarten de Jonge <mdejonge1990@gmail.com>
2015-05-27 17:09:24 +0200

    last try to fix deployment

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 ssh -p2732 kanjitrainer@maarten.sexy 'cd KanjiTrainer; git pull'
-ssh -p2732 kanjitrainer@maarten.sexy 'cd KanjiTrainer; nohup ./run.sh > /dev/null 2>&1 &'
+ssh -p2732 kanjitrainer@maarten.sexy 'nohup sh -c "( (cd KanjiTrainer; nohup ./run.sh &>/dev/null) & )"'
