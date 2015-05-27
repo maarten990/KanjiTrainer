6f60d9f4f382f0e9492eb2746726bbf861c839dc
Maarten de Jonge <mdejonge1990@gmail.com>
2015-05-27 16:59:30 +0200

    fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,1 @@
-ssh -p2732 kanjitrainer@maarten.sexy 'cd KanjiTrainer; git pull'
-ssh -p2732 kanjitrainer@maarten.sexy 'cd KanjiTrainer; ./run.sh'
+ssh -p2732 kanjitrainer@maarten.sexy 'cd KanjiTrainer; git pull; nohup ./run.sh > /dev/null 2 >&1 &'
