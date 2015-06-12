091f03d9b45895c1521d0ab87d749e432d7420bb
Maarten de Jonge <mdejonge1990@gmail.com>
2015-06-12 20:07:52 +0200

    ensure that max_strokes >= min_strokes

--- parameter_sampling.py
+++ parameter_sampling.py
@@ -1,7 +1,7 @@
 from random import choice
 from chunks import Parameters
 
-size              = [10]
+size              = [5]
 n_answers         = range(2, 7)
 max_strokes       = range(3, 11)
 min_strokes       = range(2, 5)
@@ -12,4 +12,10 @@ allow_duplicates  = [False]
 reversed_ratio    = [0, 1]
 
 def sample_parameters():
-    return Parameters(**{p: choice(globals()[p]) for p in Parameters.params()})
+    params = Parameters(**{p: choice(globals()[p]) for p in Parameters.params()})
+
+    # ensure that max_strokes >= min_strokes
+    if params.max_strokes < params.min_strokes:
+        params.max_strokes = params.min_strokes
+
+    return params
