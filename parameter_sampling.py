b7cbf9d8f090d410160e98ae4a1bb46a16da6e02
Maarten de Jonge <mdejonge1990@gmail.com>
2015-06-16 01:49:01 +0200

    fix local grade value

--- parameter_sampling.py
+++ parameter_sampling.py
@@ -7,7 +7,6 @@ max_strokes       = [0] #will be changed by min_strokes
 min_strokes       = range(1, 9)
 kanji_similarity  = [0] # currently unused
 answer_similarity = [0.0, 0.5, 1.0]
-grade             = range(1, 6)
 allow_duplicates  = [False]
 reversed_bool     = [True, False]
 question_type     = ["kanji","vocab"]
@@ -20,9 +19,9 @@ def sample_parameters(level):
         grade = range(1,3)
     if level == 3: 
         grade = range(1,6)
-    
-    
-    params = Parameters(**{p: choice(globals()[p]) for p in Parameters.params()})
+
+    params = Parameters(**{p: choice(globals()[p] if p != 'grade' else grade)
+                           for p in Parameters.params()})
     params.max_strokes = params.min_strokes + 3
     params.grade = choice(grade)
 
