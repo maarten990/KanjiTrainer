7b95726eed77741a7cd1ad38c5532b8c14f4756c
Edwin <edwin.odijk@student.uva.nl>
2015-06-16 00:00:02 +0200

    changed welcome page alignment

--- kanjitrainer.py
+++ kanjitrainer.py
@@ -41,7 +41,7 @@ chunkgen = ChunkGenerator(kanji, radicalMeanings)
 db.close()
 
 
-@app.route('/questions', methods=['GET'])
+@app.route('/questions', methods=['POST'])
 def questions():
     # check if the user already exists
     try:
