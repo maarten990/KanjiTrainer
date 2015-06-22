97081146feec713b832397fd6a3081ece31fcc9d
Rpronk91 <R.pronk91@gmail.com>
2015-06-23 00:43:55 +0200

    implemented update_parameters

--- parameter_sampling.py
+++ parameter_sampling.py
@@ -1,6 +1,7 @@
 from random import choice
 from chunks import Parameters
 
+grade             = range(1,6)
 size              = [5]
 n_answers         = range(2, 5)
 max_strokes       = [0] #will be changed by min_strokes
@@ -8,9 +9,32 @@ min_strokes       = range(1, 9)
 kanji_similarity  = [0] # currently unused
 answer_similarity = [0.0, 0.5, 1.0]
 allow_duplicates  = [False]
-reversed_bool     = [True, False]
+reversed_bool     = [False, True]
 question_type     = ["kanji","vocab"]
 
+kanji = "kanji"
+vocab = "vocab"
+
+ranking = []
+for i0 in grade:
+    for i1 in answer_similarity:
+        for i2 in n_answers:
+            for i3 in question_type:
+                for i4 in min_strokes:
+                    for i5 in reversed_bool:
+                        ranking.append([5, i2, i4, i4+3, i1, i0, i5, i3])
+
+def getRankingID(param):
+    r = ranking.index([param.size,param.n_answers, param.min_strokes, 
+                       param.max_strokes, param.answer_similarity, 
+                       param.grade, param.reversed_bool, param.question_type])
+    return r
+
+def getParametes(ID):
+    return Parameters(size=ranking[ID][0], n_answers=ranking[ID][1], 
+                      min_strokes=ranking[ID][2], max_strokes=ranking[ID][3],
+                      answer_similarity=ranking[ID][4], grade=ranking[ID][5],
+                      reversed_bool=ranking[ID][6], question_type=ranking[ID][7])
 
 def sample_parameters(level):
     if level == 1:
@@ -35,21 +59,32 @@ def safe_policy(level):
                           reversed_bool=False, question_type="kanji")
 
     if level == 2:
-        return Parameters(size=5, n_answers=3, min_strokes=1, max_strokes=6,
+        return Parameters(size=5, n_answers=3, min_strokes=3, max_strokes=6,
                           answer_similarity=0.5, grade=3,
                           reversed_bool=False, question_type="kanji")
 
     if level == 3:
-        return Parameters(size=5, n_answers=4, min_strokes=1, max_strokes=8,
+        return Parameters(size=5, n_answers=4, min_strokes=5, max_strokes=8,
                           answer_similarity=1.0, grade=6,
                           reversed_bool=False, question_type="vocab")
-
 
 def update_parameters(params, score):
     """
     Return new parameters based on the previous ones and the predicted Likert
     score.
     """
-    #TODO: implement
+    if score == 1:
+        update = choice([11,10,9])
+    elif score == 2:
+        update = choice([6,5,4])
+    elif score == 3:
+        update = choice([1,0,-1])
+    elif score == 4:
+        update = choice([-4,-5,-6])
+    else:
+        update = choice([-9,-10,-11])
+    currentRanking = getRankingID(params)
+    newRanking = max(0,min(len(ranking),currentRanking + update))   
     print('Updating parameters with score {}'.format(score))
-    return params
+    print('Now at rank:',newRanking)
+    return getParametes(newRanking)
